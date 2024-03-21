0d0c458b76d16ab410d6aeed7c57369f9f37da57
jeffbaut13 <jeferssonabv@gmail.com>
2024-03-21 10:58:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import sheetRoute from "./routes/sheet.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+// Middleware para analizar el cuerpo de la solicitud como JSON
+app.use(bodyParser.json());
+
+// Middleware CORS
+app.use(cors());
+
+// Rutas
+app.use("/api/sheet", sheetRoute);
+
+// Manejo de errores global
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor");
+});
+
+// Iniciar el servidor con nodemon
+app.listen(PORT, () => {
+  console.log(`Servidor Express iniciado en el puerto ${PORT}`);
+});
---
+++ routes/sheet.routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { google } from "googleapis";
+
+const router = express.Router();
+
+// Ruta para manejar las solicitudes POST a /sheet
+router.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+    console.log(body);
+    const auth = new google.auth.GoogleAuth({
+      keyFile: process.env.GOOGLE_SHEET_KEY_FILE, // Path to your service account key file.
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"], // Scope for Google Sheets API.
+    });
+
+    const sheets = google.sheets({
+      auth,
+      version: "v4",
+    });
+
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: "1Gh8m7B2j3J_u15SiIjruSiDoZdVj3CzTwqNYU0P8n40",
+      range: "A1",
+      valueInputOption: "USER_ENTERED",
+      requestBody: {
+        values: [[body.nombre, body.numero, body.email, body.mensaje]],
+      },
+    });
+
+    res.status(200).json({
+      message: "Datos agregados a google sheet correctamente",
+    });
+  } catch (error) {
+    // Manejo de errores
+    console.error("Error in API handler:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
+export default router;
